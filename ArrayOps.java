83395549010592085f3317627619d10010d8da42
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-11-22 15:50:11 -0500

    largest works

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,9 +1,17 @@
 public class ArrayOps {
   public static int sum(int[] arr){
     int sum=0;
-       for (int i=0; i<arr.length;i++) {
+       for (int i=0; i<arr.length; i++) {
            sum += arr[i];
        }
        return sum;
   }
+
+  public static int largest(int[] arr) {
+      int max = arr[0];
+      for (int i=0; i<arr.length; i++) {
+        max = Math.max(max, arr[i]);
+      }
+      return max;
+    }
 }
